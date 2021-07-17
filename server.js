02dd98af1a8113018184ed2badd02a6a6cfba308
rfuke <rfuke@loffacorp.com>
2021-07-17 09:30:58 -0700

    add index route

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const path = require('path');
 const express = require ('express');
 const PORT = process.env.PORT || 3002;
 const app = express();
-
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+// Express.js middleware
+app.use(express.static('public'));
 //required  note data
-const { noteData } = require('./db/db');
+//const { noteData } = require('./db/db');
 
 //add test route
 // test #1
@@ -14,18 +19,19 @@ app.get('/api/noteData', (req, res) => {
     res.send ('Hi, Robert , do you see me?');
 });
 */
-// test #2 use res.json
-
+// test #2 use db.json
+/*
 app.get('/api/noteData', (req, res) => {
     res.json(noteData);
 });
-
-// require index.html page
-/*
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-  });
 */
+
+// test 3#  display index page
+// require index.html page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'));
+  });
+
 
 // setup server port
 app.listen(PORT,() => {
